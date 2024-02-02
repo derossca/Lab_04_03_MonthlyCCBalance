609d5e020e618514724528daf4cea7c57ea19e42
Corey DeRossett <cderossett3@gmail.com>
2024-02-02 11:02:16 -0500

    Declared and initialized variables

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double creditCardBalance = 5000;
+        double INTEREST_RATE = .17;
+        double interest = creditCardBalance * INTEREST_RATE;
+        double monthOne = interest * 1;
+        double monthTwo = interest * 2;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
